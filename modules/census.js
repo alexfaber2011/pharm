8e6eec07a2c8be6c2ac968159aeec0f7e92c7877
Pari Lingampally <parilingampally@gmail.com>
2014-02-08 05:33:33 -0600

    support for all api calls

--- modules/census.js
+++ modules/census.js
@@ -2,21 +2,19 @@ var request = require('request');
 
 var USA_TODAY_KEY = "s2dakjn2ap9zbjgng5jwpacr";
 
-exports.getPopulation= function(req, res) {
-var requestBaseURL = "http://api.usatoday.com/open/census/pop?"	
+exports.getData= function(req, res) {
+var requestBaseURL = "http://api.usatoday.com/open/census/";	
+var type = req.params.type+"?"
 var keypat = req.params.keypat+"&";
 var sumlevid = req.params.sumlevid+"&";
 var api = "api_key="+USA_TODAY_KEY;
 
-var data = "err";
-
-request(requestBaseURL+keypat+sumlevid+api, function (error, response, body) {
+request(requestBaseURL+type+keypat+sumlevid+api, function (error, response, body) {
 	if (!error && response.statusCode == 200) {
-		res.send(body);
+		res.send(body); //success
 	}else {
 		console.log(error); //failed
 	}
 })
-
 
 };
