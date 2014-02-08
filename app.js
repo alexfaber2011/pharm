b1887399c7c1403b0870a024f18c1ed3dd2d67ed
Anthony To <to.anthony93@gmail.com>
2014-02-08 06:30:51 -0500

    Add yellow_pages module

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var yellow_pages = require('./modules/yellow_pages')
 var http = require('http');
 var path = require('path');
 
@@ -30,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/yellow/:what/:where', yellow_pages.getYellow);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
