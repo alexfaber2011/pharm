7d8c369ee636407e45710495ad92c1833345fb21
Alex Faber <alexfaber2011@gmail.com>
2014-02-08 05:09:16 -0600

    Zillow JSON response

--- modules/economic.js
+++ modules/economic.js
@@ -1,10 +1,19 @@
 var request = require('request');
 var parseString = require('xml2js').parseString;
 
-request('http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1bac423yk23_7x5fi&state=WA&city=Seattle&neighborhood=Ballard', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  	parseString(body, function (err, result) {
-	    console.dir(JSON.stringify(result));
-	});
-  }
-})
+
+exports.get_zillow = function(req, res){
+
+	console.log("in 'get_zillow'");
+	request('http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1bac423yk23_7x5fi&state=WA&city=Seattle&neighborhood=Ballard', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	parseString(body, function (err, result) {
+	  		res.json(result);
+		});
+	  }
+	})
+};
+
+exports.shit = function(req, res){
+	console.log("fuck");
+}
