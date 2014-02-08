375a7dae117d5be81828b06611236bb313d67934
Alex Faber <alexfaber2011@gmail.com>
2014-02-08 05:32:33 -0600

    Working on parsing JSON

--- modules/economic.js
+++ modules/economic.js
@@ -3,17 +3,13 @@ var parseString = require('xml2js').parseString;
 
 
 exports.get_zillow = function(req, res){
-
-	console.log("in 'get_zillow'");
-	request('http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1bac423yk23_7x5fi&state=WA&city=Seattle&neighborhood=Ballard', function (error, response, body) {
+	request('http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1bac423yk23_7x5fi&state=WI&city=Madison', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	  	parseString(body, function (err, result) {
+	  		json = JSON.stringify(result);
+	  		console.log(json["response"]);
 	  		res.json(result);
 		});
 	  }
 	})
 };
-
-exports.shit = function(req, res){
-	console.log("fuck");
-}
