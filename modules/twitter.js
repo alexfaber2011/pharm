0e304b7990a9a1e4292be47220ce2a5f1495b174
Alex Faber <alexfaber2011@gmail.com>
2014-02-08 23:25:24 -0600

    Commit before removing node modules

---
+++ modules/twitter.js
@@ -0,0 +1,11 @@
+//  ******Twitter API******
+
+// Example Request
+// https://api.twitter.com/1.1/search/tweets.json?q=%horse&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4
+
+// Parameters:
+// 		q 				= query string
+// 		since_id 	= since when?
+// 		max_id 		= until when?
+// 		count 		= 100
+// 		geocode 	= latitude,longitude,radius 
